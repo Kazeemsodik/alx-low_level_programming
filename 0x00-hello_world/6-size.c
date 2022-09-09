9a283369a39e07d0b35661a0e9fcfba53e3ea576
Kazeem <kazeemsodikisola@gmail.com>
2022-09-09 15:47:18 -0700

    p

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+/**
+ * main - main block
+ * return: 0
+ */
 int main(void)
 {
 	int i;
 	double d;
 	char c;
+	long int u;
+	long long l;
+	float f;
+
+
 
 	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
 	printf("The size of a double is: %u.\n", (unsigned long)sizeof(d));
 	printf("The size of a char is: %lu\n", (unsigned long) sizeof(c));
+	printf("The size of long int is: %lu\n", (long int)sizeof(u));
+	printf("The size of long long is: %lu\n", (long long)sizeof(l));
+	printf("The size of float is: %lu\n", (float)sizeof(f));
 	return(0);
 } 
