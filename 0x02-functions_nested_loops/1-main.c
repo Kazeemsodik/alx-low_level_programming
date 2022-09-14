28c735aabd5270add0cbe1c46e386a891fa029ea
Kazeem <kazeemsodikisola@gmail.com>
2022-09-13 18:26:44 -0700

    l

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* main - check the code
+* main - check the code Holberton School students.
 *
 * Return: Always 0.
 */
 int main(void)
 {
-	print_alphabet(letter);
+	print_alphabet();
 	return (0);
 }
