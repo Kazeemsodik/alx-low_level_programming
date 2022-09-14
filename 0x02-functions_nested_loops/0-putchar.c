08c0d11c738c01ef66ea011b6e763eff8c67b752
Kazeem <kazeemsodikisola@gmail.com>
2022-09-14 14:18:45 -0700

    k;

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,18 +7,14 @@
  */
 int main(void)
 {
-	char ch[8] = "_putchar";
-	int i = 0;
-	char c;
-
-	while (i <= 8)
-	{
-		c = ch[i];
-		_putchar(c);
-		i++;
-	}
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
