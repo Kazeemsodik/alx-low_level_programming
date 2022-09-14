7a3b30d0763cdca82863dbece40408b63c03fada
Kazeem <kazeemsodikisola@gmail.com>
2022-09-14 14:27:00 -0700

    i

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
-  print_alphabe  - Ent   point
- + Description: a function that prints the alphabet,
- * in lowecase followed by a new line
- * Return: 0 (Success)
- */
+* print_alphabe  - Ent   point
+* Description: a function that prints the alphabet
+* in lowecase followed by a new line
+* Return: 0 (Success)
+*/
 void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-	
+
+
 	_putchar('\n');
 }
