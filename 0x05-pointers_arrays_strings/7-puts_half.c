31d51f7dffc832f9be3b6bca4d07a31a6e4881fc
Kazeem <kazeemsodikisola@gmail.com>
2022-09-19 20:38:58 -0700

    Pointers and Arrays First Commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,12 +5,12 @@
  *
  * Return: Always 0.
  */
-
  void puts_half(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++);
+	for (i = 0; str[i] != '\0'; i++)
+		;
 
 	i++;
 	for (i /= 2; str[i] != '\0'; i++)
